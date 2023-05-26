791e99a7234bd029a6b6fc25765f12d82243488d
Kyra Atkinson <kyraatkinson@Kyras-Air-2.home>
2023-05-26 09:08:26 -0400

    Kyra Answers

--- exercise3.js
+++ exercise3.js
@@ -4,10 +4,40 @@
     // 1) An event listener that, when the element is clicked, will turn the background color to green.
     // 2) An event listener that, when the element is double-clicked, will turn the background color to blue. 
 
+    let eventListenerOne = document.getElementById("cell-one")
+
+ eventListenerOne.addEventListener("click", function(){
+eventListenerOne.style.backgroundColor = "green"
+ })
+
+ eventListenerOne.addEventListener("dblclick", function(){
+    eventListenerOne.style.backgroundColor = "blue"
+ })
+
 // Next, we'll target the #cell-two element. We will give two event listeners to this element:
     // 1) An event listener that, when the element is hovered over, will turn the border into a green, 2px width, and solidly-lined border. 
     // 2) An event listener that, when the user removes their mouse from over the element, will turn the border back into a black, 2 px width, and solidly-lined border.  
 
+    let eventListenerTwo = document.getElementById("cell-two")
+
+    eventListenerTwo.addEventListener("mouseover", function(){
+        eventListenerTwo.style.border = "2px solid green"
+         })
+        
+         eventListenerTwo.addEventListener("mouseout", function(){
+            eventListenerTwo.style.border = "2px solid black"
+         })
+
 // Lastly, we'll target the #cell-three element. We will give two event listeners to this element:
     // 1) An event listener that, when the element is clicked, will fill the element with a text of "Stop clicking me!"
     // 2) An event listener that, when the element is right-clicked, will fill the element with a text of "No right clicking either!!"
+
+    let eventListenerThree = document.getElementById("cell-three")
+
+    eventListenerThree.addEventListener("click", function(){
+        eventListenerThree.innerText = "Stop clicking me!"
+         })
+        
+         eventListenerThree.addEventListener("mouseright", function(){
+            eventListenerThree.innerText = "No right clicking either!!"
+         })
--- exercise4.js
+++ exercise4.js
@@ -3,3 +3,36 @@
 // Write some JS code below such that event listeners are attached to all the correct elements following the specifications above.
 
 // Hint: Some sort of loop may be helpful to you in this process!
+
+
+let green = document.getElementsByClassName("grid-cell green")
+
+for (let i = 0; i < green.length; i++){
+    let currGreen = green[i];
+    currGreen.addEventListener("click", function(){
+        currGreen.style.backgroundColor = "green"
+    })
+}
+
+let blue = document.getElementsByClassName("grid-cell blue")
+
+for (let i = 0; i < blue.length; i++){
+    let currBlue = blue[i];
+    currBlue.addEventListener("click", function(){
+        currBlue.style.backgroundColor = "blue"
+    })
+}
+
+
+
+let red = document.getElementsByClassName("grid-cell red")
+
+for (let i = 0; i < red.length; i++){
+    let currRed = red[i];
+    currRed.addEventListener("click", function() {
+        currRed.style.backgroundColor = "red"
+    })
+}
+
+
+
